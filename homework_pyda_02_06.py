9d0c5aed04a547a69edd194af8db686cd171ad6f
neur0tr0n <neur0tr0n@icloud.com>
2020-11-22 20:54:33 +0300

    20201122-1416

--- homework_pyda_02_06.py
+++ homework_pyda_02_06.py
@@ -1,7 +1,10 @@
 # Задание 6 (необязательное)
 number_string = input('Введите числа через пробел: ')
 num_list = number_string.split(' ')
-unique_num = set(num_list)
+unique_num = []
+for num in num_list:
+    if num not in unique_num:
+        unique_num.append(num)
 for num in unique_num:
     if num_list.count(num) > 1:
         print(num, end=' ')
