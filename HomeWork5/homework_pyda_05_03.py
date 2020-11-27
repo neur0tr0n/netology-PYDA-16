9763217d686ea14333aac73509a38d236857358b
neur0tr0n <neur0tr0n@icloud.com>
2020-11-27 08:25:16 +0300

    20201126-2006

--- HomeWork5/homework_pyda_05_03.py
+++ HomeWork5/homework_pyda_05_03.py
@@ -2,7 +2,7 @@ import json
 
 log_file = open('/Users/neur0tr0n/Downloads/Downloads/visit_log.csv', 'r')
 purchase_file = open('/Users/neur0tr0n/Downloads/Downloads/purchase_log.txt', 'r')
-funnel_file = open('/Users/neur0tr0n/Downloads/Downloads/funnel_1.csv', 'w')
+funnel_file = open('/Users/neur0tr0n/Downloads/Downloads/funnel_2.csv', 'w')
 purchases = {}
 purchase = {}
 purchases_len = 0
@@ -10,20 +10,17 @@ for line in purchase_file:
     purchase = json.loads(line)
     purchases[purchase['user_id']] = purchase['category']
 purchase_file.close()
+purchases_len = len(purchases)
+quantization = round(purchases_len / 1000)
 count = 0
-for i in log_file:
-    count += 1
-log_len = count
-log_file.seek(0)
-quantization = round(log_len / 1000)
-count = 0
-for line in log_file:
-    item = line.strip().split(',')
-    for uid, cat in purchases.items():
+for uid, cat in purchases.items():
+    log_file.seek(0)
+    for line in log_file:
+        item = line.strip().split(',')
         if uid == item[0]:
             funnel_file.write(f'{item[0]},{item[1]},{cat}\n')
             break
     if count % quantization == 0:
-        print(f'Выполнено: {round(count / log_len * 100, 2)} %')
+        print(f'Выполнено: {round(count / purchases_len * 100, 2)} %')
     count += 1
 funnel_file.close()
