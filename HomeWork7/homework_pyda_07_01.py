94fc50a6afb13baaa352f012a159fad39c255f4f
neur0tr0n <neur0tr0n@icloud.com>
2020-12-06 17:02:18 +0300

    20201206-1508

--- HomeWork7/homework_pyda_07_01.py
+++ HomeWork7/homework_pyda_07_01.py
@@ -1,4 +1,7 @@
 # Задание 1
+# Напишите функцию, которая возвращает название валюты (поле ‘Name’) с
+# максимальным значением курса с помощью сервиса
+# https://www.cbr-xml-daily.ru/daily_json.js
 import requests
 
 
@@ -6,7 +9,7 @@ def get_max_value_cur():
     cur_lib = {}
     max_val = 0
     max_val_cur = ''
-    req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    req = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
     cur_lib = req.json()['Valute']
     for cur, lib_ in cur_lib.items():
         if max_val <= lib_['Value']:
@@ -18,6 +21,7 @@ def get_max_value_cur():
 def get_cur_values():
         req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         return req.json()['Valute']
+
 
 print(get_cur_values())
 print(get_max_value_cur())
