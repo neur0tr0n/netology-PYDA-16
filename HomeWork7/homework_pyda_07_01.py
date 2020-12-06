394280322f247bf2cab77702edd94aad8bbc97ab
neur0tr0n <neur0tr0n@icloud.com>
2020-12-06 15:08:59 +0300

    20201206-1508

---
+++ HomeWork7/homework_pyda_07_01.py
@@ -0,0 +1,27 @@
+# Задание 1
+import requests
+
+
+def get_max_value_cur():
+    cur_lib = {}
+    max_val = 0
+    max_val_cur = ''
+    req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    cur_lib = req.json()['Valute']
+    for cur, lib_ in cur_lib.items():
+        if max_val <= lib_['Value']:
+            max_val = lib_['Value']
+            max_val_cur = cur
+    return max_val_cur
+
+
+def get_cur_values():
+        req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        return req.json()['Valute']
+
+print(get_cur_values())
+print(get_max_value_cur())
+
+
+
+
