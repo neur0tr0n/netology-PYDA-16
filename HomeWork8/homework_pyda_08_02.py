ced7a48d228ba12fbc4239b0738c125b9d2d343f
neur0tr0n <neur0tr0n@icloud.com>
2020-12-18 18:54:46 +0300

    20201217-1911

--- HomeWork8/homework_pyda_08_02.py
+++ HomeWork8/homework_pyda_08_02.py
@@ -4,6 +4,14 @@
 import re
 some_string = 'Напишите функцию функцию, которая будет будет будет будет удалять все все все все последовательные повторы слов из из из из заданной строки строки при помощи регулярных выражений.'
 
-reg_exp = re.compile(r'')
+reg_exp = re.compile(r'\w+')
+new_line = ''
+prev_item = ''
+for item in reg_exp.findall(some_string):
+    if item != prev_item:
+        new_line = new_line + ' ' + item
+    prev_item = item
+new_line = new_line.strip()
+print(new_line)
 
 
