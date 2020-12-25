1248572eef38660760e7a36a10d81a7435c45737
neur0tr0n <neur0tr0n@icloud.com>
2020-12-25 23:15:38 +0300

    20201224-2229

--- HomeWork10/homework_pyda_10_02.py
+++ HomeWork10/homework_pyda_10_02.py
@@ -3,9 +3,14 @@
 # категорий 4, 12 и 21 за период с 2005 по 2010 года. Не учитывайте в расчетах отрицательные значения quantity.
 import pandas as pd
 
-countries = pd.read_csv('/Users/neur0tr0n/Downloads/10/power.csv')
-filtered_countries = countries[['country', 'quantity', 'category', 'year']]\
-    .query('(country == \'Estonia\' or country == \'Lithuania\' or country == \'Lithuania\')  '
-             'and (category == 4 or category == 12 or category == 21) and quantity > 0 '
-             'and year > 2005 and year < 2010')
-print('Суммарное потребление стран Прибалтики (Латвия, Литва и Эстония): {}'.format(filtered_countries['quantity'].sum()))
+
+def get_sum_consume():
+    countries = pd.read_csv('/Users/neur0tr0n/Downloads/10/power.csv')
+    query_string = '(country == \'Estonia\' or country == \'Lithuania\' or country == \'Lithuania\') ' \
+                   'and (category == 4 or category == 12 or category == 21) and quantity > 0 ' \
+                   'and year > 2005 and year < 2010'
+    filtered_countries = countries[['country', 'quantity', 'category', 'year']].query(query_string)
+    return filtered_countries['quantity'].sum()
+
+
+print('Суммарное потребление стран Прибалтики (Латвия, Литва и Эстония): {}'.format(get_sum_consume()))
