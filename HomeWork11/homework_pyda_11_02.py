b1e67c3101b703076c4f5d4bbcb7f72bd7780665
neur0tr0n <neur0tr0n@icloud.com>
2020-12-30 15:57:41 +0300

    20201230-1500

---
+++ HomeWork11/homework_pyda_11_02.py
@@ -0,0 +1,28 @@
+# Задание 2
+# Используем файл keywords.csv.
+# Необходимо написать гео-классификатор, который каждой строке сможет выставить географическую
+# принадлежность определенному региону. Т. е. если поисковый запрос содержит название города региона,
+# то в столбце ‘region’ пишется название этого региона. Если поисковый запрос не содержит названия города,
+# то ставим ‘undefined’
+import pandas as pd
+
+
+def get_region(row):
+    region = 'undefined'
+    geo_data = {
+        'Центр': ['москва', 'тула', 'ярославль'],
+        'Северо-Запад': ['петербург', 'псков', 'мурманск'],
+        'Дальний Восток': ['владивосток', 'сахалин', 'хабаровск']
+    }
+    for key, cities in geo_data.items():
+        split_keywords = [x.lower() for x in row['keyword'].split()]
+        if set(split_keywords).intersection(cities):
+            region = key
+    return region
+
+
+in_file = '/Users/neur0tr0n/Downloads/ml-latest-small-3/keywords.csv'
+out_file = '/Users/neur0tr0n/Downloads/ml-latest-small-3/report.csv'
+keywords = pd.read_csv(in_file)
+keywords['region'] = keywords.apply(get_region, axis=1)
+print(keywords.query('region != \'undefined\'').head(10))
