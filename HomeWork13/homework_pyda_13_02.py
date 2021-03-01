833ad83d405904d2dfa32e773777823235383f98
neur0tr0n <neur0tr0n@icloud.com>
2021-03-01 23:01:22 +0300

    20210228-15:34

--- HomeWork13/homework_pyda_13_02.py
+++ HomeWork13/homework_pyda_13_02.py
@@ -6,12 +6,39 @@
 # В итоге должен формироваться датафрейм со столбцами: <почта> - <дата утечки> - <источник утечки> - <описание утечки>
 # Подсказка: сервис работает при помощи "скрытого" API. Внимательно изучите post-запросы.
 import requests
-from bs4 import BeautifulSoup as bs
-email_list = ['bstrygin@bk.ru', 'neur0tr0n@me.com', 'boris.srygin@gmail.com']
-url = 'https://www.avast.com/hackcheck/'
-params = {}
-resp = requests.get(url, params=params)
-soup = bs(resp.text, 'html.parser')
-print(soup)
+import json
+import pandas as pd
 
+response = {}
+emails = ['neur0tr0n@me.com', 'boris.strygin@gmail.com', 'bstrygin@bk.ru']
+url = 'https://identityprotection.avast.com/v1/web/query/site-breaches/unauthorized-data'
+data = {'emailAddresses' : ['neur0tr0n@me.com', 'boris.strygin@gmail.com', 'bstrygin@bk.ru']}
+headers = {
+    'Content-Type': 'application/json;charset=utf-8',
+    'Accept': 'text/plain',
+    'Vaar-Header-App-Product-Name': 'hackcheck-web-avast',
+    'Vaar-Version': '0',
+    'Vaar-Header-App-Product': 'hackcheck-web-avast',
+    'Vaar-Header-App-Build-Version': '1.0.0'
+}
+res = requests.post(url, headers=headers, data=json.dumps(data))
+
+print(res.status_code)
+if res.status_code == 200 and res.json():
+    response = res.json()
+    email_dictionary = response['summary']
+    d_frame = pd.DataFrame()
+    for email in emails:
+        if email_dictionary[email]['breaches']:
+            breaches = email_dictionary[email]['breaches']
+            for breach in breaches:
+                breach_dictionary = response['breaches'][str(breach)]
+                publishDate = breach_dictionary['publishDate']
+                site = breach_dictionary['site']
+                description = breach_dictionary['description']
+                row = {'email': email, 'publishDate': publishDate, 'site': site, 'description': description}
+                d_frame = pd.concat([d_frame, pd.DataFrame([row])])
+    print(d_frame)
+else:
+    print('Request has returned none!')
 
