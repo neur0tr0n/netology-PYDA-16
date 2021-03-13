6ef76274eeeeac0fc3415c5ba9595e6d91879acf
neur0tr0n <neur0tr0n@icloud.com>
2021-03-13 23:44:50 +0300

    20210313-22:52

--- HomeWork17/homework_pyda_17_01.py
+++ HomeWork17/homework_pyda_17_01.py
@@ -6,16 +6,43 @@
 # Вывести график остатков
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('water.csv')
-df.corr()
-print(df.info())
-df.plot(kind='scatter', x='mortality', y='hardness')
+
+# Построение точечного графика
+plt.scatter(df['hardness'], df['mortality'])
+plt.xlabel('mortality')
+plt.ylabel('hardness')
 plt.show()
 
+# Расчет коэффициентов корреляции
 corr = df[['mortality', 'hardness']].corr()['hardness']['mortality']
 print(f'Корреляция (Пирсон): {corr:.2}')
 corr_spearman = corr = df[['mortality', 'hardness']].corr(method='spearman')['hardness']['mortality']
 print(f'Корреляция (Спирмен): {corr_spearman:.2}')
 
+# Построение линейной регрессионной модели и вычисление коэффициента детерминации
+x = df[['hardness']]
+y = df['mortality']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+model = LinearRegression()
+model.fit(x_train, y_train)
+print(f'Параметры модели: a = {model.coef_}, b = {model.intercept_}')
+y_pedict = model.predict(x_test)
+determinantion = model.score(x_test, y_test)
+print(f'Коэффициент детерминации: {determinantion}')
+plt.scatter(x_test, y_test)
+plt.plot(x_test, y_pedict, color='r')
+plt.xlabel('hardness')
+plt.ylabel('mortality')
+plt.show()
+
+# График остатков
+plt.scatter(x_test, y_pedict - y_test)
+plt.xlabel('hardness')
+plt.ylabel('mortality')
+plt.show()
