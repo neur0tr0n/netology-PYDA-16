d6dce6a4393ff100114d7e1a2c281a06928b6a63
neur0tr0n <neur0tr0n@icloud.com>
2021-03-13 12:38:15 +0300

    20210312-21:08

--- HomeWork16/homework_pyda_16_01.py
+++ HomeWork16/homework_pyda_16_01.py
@@ -3,8 +3,15 @@
 # после чего выпадает случайное целое число, равномерно распределенное от 0 до 999.
 # При некоторых значениях числа игрок получает выигрыш (см. справа)
 # Выгодна ли игра игроку?
-import numpy as np
 import random
+import re
+import pandas as pd
+import matplotlib as plt
+
+# Колесо автомата с числами
+AUTOMATE_PIN = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# Количество одновременно выпадающих чисел автомата
+AUTOMATE_PATTERN = 3
 prizes = {
     r'777': 200,
     r'999': 100,
@@ -17,13 +24,12 @@ prizes = {
     r'\d{2}0': 1
 }
 # Число попыток игрока
-user_trials = 10
+user_trials = 1000
 # Банк игрока
 user_bank = 100
-# Колесо автомата с числами
-AUTOMATE_PIN = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# Количество одновременно выпадающих чисел автомата
-AUTOMATE_PATTERN = 3
+# Стоимость ставки
+bet = 1
+
 
 def make_trial():
     pattern = ''
@@ -31,7 +37,33 @@ def make_trial():
         pattern = pattern + AUTOMATE_PIN[random.randint(0, len(AUTOMATE_PIN) - 1)]
     return pattern
 
+
+def get_prize(_trial):
+    ret_val = 0
+    for _prize, val in prizes.items():
+        reg_exp = re.compile(_prize)
+        if reg_exp.findall(_trial):
+            ret_val = val
+            break
+    return ret_val
+
+
+trial_count = 0
+row = {}
+df = pd.DataFrame()
 for i in range(user_trials):
-    print(make_trial())
+    if user_bank > 0:
+        trial_count += 1
+        user_bank -= bet
+        trial = make_trial()
+        prize = get_prize(trial)
+        user_bank += prize
+    else:
+        break
+    row = {'trial_count': trial_count, 'user_bank': user_bank, 'trial': trial, 'prize': prize}
+    df = pd.concat([df, pd.DataFrame([row])])
+    print(f'Номер ставки: {trial_count}\tБанк игрока: {user_bank}\tВыпавшие числа: {trial}\tВыигрыш: {prize}')
+
+print(df)
 
 
