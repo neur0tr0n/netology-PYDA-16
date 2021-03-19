5f443e3c47d9507a0a5ad4a234e647c60f5bb478
neur0tr0n <neur0tr0n@icloud.com>
2021-03-19 20:14:30 +0300

    20210319-20:14

--- HomeWork18/homework_pyda_18_01.py
+++ HomeWork18/homework_pyda_18_01.py
@@ -8,6 +8,7 @@
 # Подберите оптимальное число кластеров для алгоритма kmeans и визуализируйте полученную кластеризацию
 
 import  matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.datasets import load_iris
@@ -25,31 +26,53 @@ cmap = colors.LinearSegmentedColormap(
 plt.cm.register_cmap(cmap=cmap)
 iris = load_iris()
 variety = iris.target
-
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 plt.scatter(df['sepal length (cm)'], df['sepal width (cm)'], c=variety)
 plt.xlabel('sepal length (cm)')
 plt.ylabel('sepal width (cm)')
+plt.title('Распределение классов')
 plt.show()
 
-x_train, x_test, y_train, y_test = train_test_split(df, variety, train_size=0.25)
+
+x_train, x_test, y_train, y_test = train_test_split(df, variety, test_size=0.33, random_state=42)
 lda = LinearDiscriminantAnalysis()
 lda.fit(x_train, y_train)
 y_predict = lda.predict(x_test)
 result = pd.DataFrame([y_test, y_predict]).T
 print(result)
-print(accuracy_score(y_test, y_predict))
+accuracy = accuracy_score(y_test, y_predict)
+print(accuracy)
 print(lda.coef_)
-plt.scatter(x_train['sepal length (cm)'], x_train['sepal width (cm)'], c=y_train)
+plt.scatter(x_test['sepal length (cm)'], x_test['sepal width (cm)'], c=y_predict)
 plt.xlabel('sepal length (cm)')
 plt.ylabel('sepal width (cm)')
+plt.title(f'Классификация модели. (Точность: {accuracy})')
 plt.show()
-
-plt.title('Ошибка предсказания')
+plt.title(f'Ошибка предсказания.(Точность: {accuracy})')
 plt.scatter(x_test['sepal length (cm)'], x_test['sepal width (cm)'], c=y_test)
 plt.scatter(x_test['sepal length (cm)'], x_test['sepal width (cm)'], c=abs(y_predict - y_test))
 plt.xlabel('sepal length (cm)')
 plt.ylabel('sepal width (cm)')
 plt.show()
 
+plt.scatter(x_test['sepal length (cm)'], x_test['sepal width (cm)'], c=y_predict)
+plt.xlabel('sepal length (cm)')
+plt.ylabel('sepal width (cm)')
+plt.title(f'Классификация модели. Центроиды')
+centers = lda.means_
+plt.scatter(centers[:, 0], centers[:, 1], c='r', s=150, marker='*')
+
+nx, ny = 200, 100
+x_min, x_max = plt.xlim()
+y_min, y_max = plt.ylim()
+xx, yy = np.meshgrid(np.linspace(x_min, x_max, nx), np.linspace(y_min, y_max, ny))
+
+Z = lda.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+Z = Z[:, 1].reshape(xx.shape)
+
+#plt.pcolormesh(xx, yy, Z, cmap='red_blue_classes', norm=colors.Normalize(0., 1.), zorder=-1)
+#plt.contour(xx, yy, Z, [0.5], linewidths=2., colors='white')
+plt.contour(centers[:, 0], centers[:, 1], Z, [0.5], linewidth=2, colors='green')
+print(y_predict.shape)
+plt.show()
 
